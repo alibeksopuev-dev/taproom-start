5163a80f4ec4599d58e2ad392a53ecd21700b752
alibeksopu <a.sopuev@itez.com>
2026-04-23 16:49:21 +0700

    nitro

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
 import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
-
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { nitro } from 'nitro/vite'
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
-  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
+  plugins: [
+    tailwindcss(),
+    tanstackStart(),
+    nitro({ preset: 'vercel' }),
+    viteReact(),
+  ],
 })
 
 export default config
